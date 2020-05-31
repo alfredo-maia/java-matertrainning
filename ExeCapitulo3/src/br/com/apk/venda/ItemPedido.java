9eefe6d058e947105260d8320e01611f0532165b
alfredo-maia <alfredosouza100@gmail.com>
2020-05-31 16:32:12 -0300

    Adicionando Documentação JavaDoc em HTML

--- ExeCapitulo3/src/br/com/apk/venda/ItemPedido.java
+++ ExeCapitulo3/src/br/com/apk/venda/ItemPedido.java
@@ -4,25 +4,40 @@ package br.com.apk.venda;
 import br.com.apk.estoque.Produto;
 
 /**
- *
- * @author alfre
+ * Clase do item do produto
+ * @author Alfredo
  */
 public class ItemPedido {
     private int quantidade;
     private Produto produto;
-
+    /**
+     * Retorna a quantidade negociada
+     * @return 
+     */
     public int getQuantidade() {
         return quantidade;
     }
-
+    
+    /**
+     * Atualiza a quantidade negociada 
+     * @param quantidade 
+     */
     public void setQuantidade(int quantidade) {
         this.quantidade = quantidade;
     }
-
+    
+    /**
+     * Busca o código do produto
+     * @return 
+     */
     public Produto getProduto() {
         return produto;
     }
-
+    
+    /**
+     * Atualiza o produto
+     * @param produto 
+     */
     public void setProduto(Produto produto) {
         this.produto = produto;
     }
