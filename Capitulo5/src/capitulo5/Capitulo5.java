0ecd19d3efdb7dde354ca1011ef4fce921940c8d
alfredo-maia <alfredosouza100@gmail.com>
2020-06-05 19:11:29 -0300

    Adicionando o capítulo 5, base de todo java

---
+++ Capitulo5/src/capitulo5/Capitulo5.java
@@ -0,0 +1,17 @@
+
+package capitulo5;
+
+/**
+ *
+ * @author Alfredo
+ */
+public class Capitulo5 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+            System.out.println("Classe Principal");
+    }
+    
+}
